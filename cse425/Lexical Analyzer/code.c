e3879ceb7a22ed388468e65e15fe60b403fcd0be
Sajan Sarker <sajansarker47@gmail.com>
2024-04-13 13:54:36 +0600

    input file for lexical_analyzer.py

--- cse425/Lexical Analyzer/code.c
+++ cse425/Lexical Analyzer/code.c
@@ -1,14 +1,2 @@
-// Input of the lexical_analyzer.py file
-#include <stdio.h>
-
-int sum(int a, int b) {
-    return a + b;
-}
-
-void main() {
-    int num1 = 3, num2 = 5, result;
-    
-    result = sum(num1, num2);
-    
-    printf("The sum is: %d\n", result);
-}
+int a = b + c   // Expression 1
+int x=ab+bc+30+switch+ 0y    // Expression 2
