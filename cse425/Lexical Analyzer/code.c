fecb2dda7394de796308dc97dc1a8df541322256
Sajan Sarker <sajansarker47@gmail.com>
2024-04-19 21:57:00 +0600

    input of lexical_analyzer.py file

--- cse425/Lexical Analyzer/code.c
+++ cse425/Lexical Analyzer/code.c
@@ -1,2 +1,3 @@
-int a = b + c   // Expression 1
-int x=ab+bc+30+switch+ 0y    // Expression 2
+/*** File Name: code.c  ***/
+int a = b + c;   // Expression 1
+int x = ab + bc - 30 + previous / 0y;    // Expression 2
