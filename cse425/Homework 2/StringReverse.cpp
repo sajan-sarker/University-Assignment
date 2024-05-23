e0d0ac488c2fab5190174b30a63205f0500e3843
Sajan Sarker <sajansarker47@gmail.com>
2024-05-23 20:34:07 +0600

    Source code documented for ans 3

--- cse425/Homework 2/StringReverse.cpp
+++ cse425/Homework 2/StringReverse.cpp
@@ -2,21 +2,22 @@
 #include<iostream>
 using namespace std;
 
-void reverseString(string statement, int index){
-    if(index == -1){
-        return;
+void reverseString(string statement, int index){    // Function to reverse the statement
+    if(index == -1){    // Check if the index is -1
+        return; // Return if the index is -1
     }
-    cout << statement[index];
-    reverseString(statement, index - 1);
+    cout << statement[index];   // Print the character at the index
+    reverseString(statement, index - 1);    // Call the function recursively with the decremented index
 }
 
-int main(){
-    string statement;
-    cout << "Enter a statement for reverse: ";
-    getline(cin, statement);
+int main(){ // Main function
+    string statement;   // Variable to store the statement
+    cout << "Enter a statement for reverse: ";  // Ask the user to enter a statement
+    getline(cin, statement);    // Get the statement from the user
 
-    int len = size(statement) - 1;
+    int len = size(statement) - 1;  // Variable to store the length of the statement
 
-    reverseString(statement, len);
+    cout << "Reverse of the statement is: ";    // Print the message
+    reverseString(statement, len);  // Call the function to reverse the statement
     return 0;
 }
