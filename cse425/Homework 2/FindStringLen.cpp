0a39bb17de4a8c328f8ef964ee660659054ceee6
Sajan Sarker <sajansarker47@gmail.com>
2024-05-23 20:33:32 +0600

    Source code documented for ans 1

--- cse425/Homework 2/FindStringLen.cpp
+++ cse425/Homework 2/FindStringLen.cpp
@@ -2,23 +2,22 @@
 #include<iostream>
 using namespace std;
 
-void getLen(string statement){
-    int size = 0;
-    for(int i = 0; statement[i] != '\0'; i++){
-        size ++;
+void getLen(string statement){ // Function to get the length of the string
+    int size = 0;   // Variable to store the length of the string
+    for(int i = 0; statement[i] != '\0'; i++){  // Loop to iterate through the string
+        size ++;    // Increment the size variable
     }
-    cout << "Given Statement is: " << statement << endl;
-    cout << "Length of the statement is: " << size;
+    cout << "Given Statement is: " << statement << endl;    // Print the statement
+    cout << "Length of the statement is: " << size; // Print the length of the statement
 }
 
-int main(){
-    string statement;
-    cout << "Enter a statement: ";
-    getline(cin, statement);
-    cout << endl;
+int main(){ // Main function
+    string statement;   // Variable to store the statement
+    cout << "Enter a statement: ";  // Ask the user to enter a statement
+    getline(cin, statement);    // Get the statement from the user
+    cout << endl;   // Print a new line
 
-    getLen(statement);
+    getLen(statement);  // Call the function to get the length of the statement
 
-    return 0;
+    return 0;   
 }
-
